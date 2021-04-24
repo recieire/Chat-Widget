918b84bb77c169d599039d829f9a6e9963dece9c
Recieire Carbelim <recieiremarcos@gmail.com>
2021-04-24 00:24:47 -0300

    some refactoring on routes.ts

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,11 @@
-import express from 'express';
+import express from "express";
+import "./database";
+import { routes } from "./routes";
 
 const app = express();
 
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  return res.json({
-    message: "Olá NLW 05!"
-  })
-})
+app.use(routes);
 
-app.post('/', (req, res) => {
-  return res.json({
-    message: "Usuário criado com sucesso!"
-  })
-})
-
-
-app.listen(3333, () => console.log("Server is running on port 3333") )
+app.listen(3333, () => console.log("Server is running on port 3333"));
