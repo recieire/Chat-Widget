03eefd9227fa7fecf82bb87ca33fc07a69841ccf
Recieire Carbelim <recieiremarcos@gmail.com>
2021-05-02 23:16:40 -0300

    major changes in chat, admin and client files

--- src/repositories/ConnectionsRepository.ts
+++ src/repositories/ConnectionsRepository.ts
@@ -1,4 +1,4 @@
-import { Entity, EntityRepository, Repository } from "typeorm";
+import { EntityRepository, Repository } from "typeorm";
 import { Connection } from "../entities/Connection";
 
 @EntityRepository(Connection)
